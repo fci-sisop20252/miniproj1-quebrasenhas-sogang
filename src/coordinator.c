dd60f0b90bfb4a756b2e47822f3510e7398ed266
7-lima <rafael.lima110305@gmail.com>
2025-09-10 12:33:25 -0300

    Update coordinator.c meu deus tomara q n exploda

--- src/coordinator.c
+++ src/coordinator.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <time.h>
+#include <errno.h>
 #include "hash_utils.h"
 
 /**
@@ -69,14 +70,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
-    // Parsing dos argumentos
+
     const char *target_hash = argv[1];
     int password_len = atoi(argv[2]);
     const char *charset = argv[3];
     int num_workers = atoi(argv[4]);
     int charset_len = strlen(charset);
     
-    // Validações dos parâmetros
+
     if (password_len < 1 || password_len > 10) {
         fprintf(stderr, "Erro: Tamanho da senha deve estar entre 1 e 10\n");
         return 1;
@@ -98,41 +99,40 @@ int main(int argc, char *argv[]) {
     printf("Charset: %s (tamanho: %d)\n", charset, charset_len);
     printf("Número de workers: %d\n", num_workers);
     
-    // Calcular espaço de busca total
+
     long long total_space = calculate_search_space(charset_len, password_len);
     printf("Espaço de busca total: %lld combinações\n\n", total_space);
     
-    // Remover arquivo de resultado anterior se existir
+
     unlink(RESULT_FILE);
     
-    // Registrar tempo de início
     time_t start_time = time(NULL);
     
     // TODO 2: Dividir o espaço de busca entre os workers
     long long passwords_per_worker = total_space / num_workers;
     long long remaining = total_space % num_workers;
     
-    // Arrays para armazenar PIDs dos workers
+
     pid_t workers[MAX_WORKERS];
     
     // TODO 3: Criar os processos workers usando fork()
     printf("Iniciando workers...\n");
     
     for (int i = 0; i < num_workers; i++) {
-        // Calcular intervalo de senhas para este worker
+
         long long start_index = i * passwords_per_worker;
         long long end_index = (i == num_workers - 1) ? 
                              (start_index + passwords_per_worker + remaining - 1) : 
                              (start_index + passwords_per_worker - 1);
 
-        // Converter indices para senhas de inicio e fim
+
         char start_password[password_len + 1];
         char end_password[password_len + 1];
         
         index_to_password(start_index, charset, charset_len, password_len, start_password);
         index_to_password(end_index, charset, charset_len, password_len, end_password);
         
-        // Usar fork() para criar processo filho
+
         pid_t pid = fork();
         
         if (pid == -1) {
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
         }
         
         if (pid == 0) {
-            // No processo filho: usar execl() para executar worker
+
             char worker_id[10];
             snprintf(worker_id, sizeof(worker_id), "%d", i);
             
@@ -156,11 +156,11 @@ int main(int argc, char *argv[]) {
             execl("./worker", "./worker", target_hash, 
                   start_password, end_password, charset, argv[2], worker_id, NULL);
             
-            // Se execl falhar
+
             perror("Erro ao executar worker");
             exit(1);
         } else {
-            // No processo pai: armazenar PID
+
             workers[i] = pid;
             printf("Worker %d iniciado com PID %d\n", i, pid);
         }
@@ -171,17 +171,18 @@ int main(int argc, char *argv[]) {
     // TODO 8: Aguardar todos os workers terminarem usando wait()
     int workers_completed = 0;
     int workers_found_password = 0;
-    
     for (int i = 0; i < num_workers; i++) {
         int status;
         pid_t finished_pid = wait(&status);
         
         if (finished_pid == -1) {
+            if (errno == ECHILD) {
+                break;
+            }
             perror("Erro ao aguardar processo filho");
             continue;
         }
         
-        // Identificar qual worker terminou
         for (int j = 0; j < num_workers; j++) {
             if (workers[j] == finished_pid) {
                 if (WIFEXITED(status)) {
@@ -202,8 +203,10 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+
+    while (waitpid(-1, NULL, WNOHANG) > 0) {
+    }
     
-    // Registrar tempo de fim
     time_t end_time = time(NULL);
     double elapsed_time = difftime(end_time, start_time);
     
@@ -253,10 +256,6 @@ int main(int argc, char *argv[]) {
     }
     printf("Workers que completaram: %d/%d\n", workers_completed, num_workers);
     printf("Workers que encontraram a senha: %d\n", workers_found_password);
-    
-    int child_status;
-    while (waitpid(-1, &child_status, WNOHANG) > 0) {
-    }
     
     return 0;
 }
